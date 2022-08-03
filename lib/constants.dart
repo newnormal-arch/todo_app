e917dc99c81b987c2f7707aa1e040ca0e5fff8b7
Bongz Podolski <ntshingilab@outlook.com>
2022-08-03 14:43:35 +0200

    first commit

---
+++ lib/constants.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// COLOR CONSTANTS
+const primaryColor = Color(0xffF4C27F);
+const primaryGradientColor = LinearGradient(
+  colors: [
+    Color(0xffD8605B),
+    Color(0xffF4C27F),
+  ],
+);
+
+// TEXT CONSTANTS
+
+const primaryTextColor = Colors.black;
+final primaryTextOpacityColor = Colors.black.withOpacity(.74);
+const primaryButtonTextColor = Colors.white;
+const primaryRedTextColor = Color(0xffD8605B);
